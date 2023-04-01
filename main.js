3d6921b64c1353a8652bef4dfc4ed5dce99074aa
sekar004845 <sekar004845@gmail.com>
2023-03-31 18:09:39 -0700

    update

---
+++ main.js
@@ -0,0 +1,41 @@
+$('.row-8').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+	autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+// const hamburger = document.querySelector('.hamburger');
+// const nav = document.querySelector('.nav');
+
+// hamburger.addEventListener('click', () => {
+// nav.classList.toggle('hide');
+// });
+// let body=document.querySelector("body");
+// let btn=document.getElementById("btn");
+// btn.addEventListener('click',function(){
+//     body.classList.toggle("navbar")
+// }); 
+// function add(){
+//     let nav=document.querySelector(".navul");
+//     nav.classList.toggle("mystyle");
+//     console.log("hi")
+// }
+
+// var icon=document.getElementById("icon");
+// function navbar(){
+//     var nav=document.getElementById("nav");
+//     nav.classList.toggle("active")
+// }
+// icon.addEventListener("click",navbar)
